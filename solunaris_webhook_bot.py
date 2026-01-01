f2b8f8e822e2bef1b51431548b77e143e6bf3421
jrobinson2112-cmyk <jrobinson2112@hotmail.co.uk>
2026-01-01 22:02:17 +0000

    Update solunaris_webhook_bot.py

--- solunaris_webhook_bot.py
+++ solunaris_webhook_bot.py
@@ -23,7 +23,7 @@ if not WEBHOOK_URL:
 
 CALIBRATE_ROLE_ID = 1439069787207766076
 
-UPDATE_INTERVAL = int(os.getenv("UPDATE_INTERVAL", "30"))  # seconds
+UPDATE_INTERVAL = int(os.getenv("UPDATE_INTERVAL", "4.7"))  # seconds
 
 # Measured conversion:
 # 20 in-game minutes = 94 real seconds => 1 in-game minute = 4.7 real seconds
