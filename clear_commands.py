8da78a7dfe3a479dc4214d41efc9a3a34a300a58
jrobinson2112-cmyk <jrobinson2112@hotmail.co.uk>
2026-01-01 19:08:54 +0000

    Update clear_commands.py

--- clear_commands.py
+++ clear_commands.py
@@ -1,10 +1,12 @@
-import discord
 import os
+import discord
 from dotenv import load_dotenv
 
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+if not TOKEN:
+    raise RuntimeError("DISCORD_TOKEN missing")
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -12,10 +14,13 @@ tree = discord.app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
-    print("Clearing ALL global commands...")
+    print("Clearing ALL global commands...", flush=True)
+
+    # Remove all global commands:
     tree.clear_commands(guild=None)
     await tree.sync()
-    print("DONE. You can now stop this script.")
+
+    print("DONE. Global commands cleared.", flush=True)
     await client.close()
 
 client.run(TOKEN)
